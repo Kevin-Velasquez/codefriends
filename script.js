c4250ba61a77aa0f0a66c650a14704b5bf5ebc39
Kevin-Velasquez <kevinx23x@yahoo.com>
2017-09-14 02:39:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ var subtext = document.getElementById("subtext");
 var images = [
 	'http://4.bp.blogspot.com/-btXyUVef-ps/TsnxFxt-A6I/AAAAAAAABLs/2qFK-7dLjW4/s1600/nick-brandt-wildlife-animal-black-and-white-portraits-buffalo-group-portrait-amboseli-2006.jpg',
 	'http://tailandfur.com/wp-content/uploads/2014/09/beautiful-and-cute-animals-wallpaper-2.jpg',
-    'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/caracal-run.ngsversion.1435834128807.adapt.1900.1.jpg',
-    'https://static.pexels.com/photos/86462/red-kite-bird-of-prey-milan-raptor-86462.jpeg',
-    'http://4.bp.blogspot.com/-btXyUVef-ps/TsnxFxt-A6I/AAAAAAAABLs/2qFK-7dLjW4/s1600/nick-brandt-wildlife-animal-black-and-white-portraits-buffalo-group-portrait-amboseli-2006.jpg'
+        'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/caracal-run.ngsversion.1435834128807.adapt.1900.1.jpg',
+        'https://static.pexels.com/photos/86462/red-kite-bird-of-prey-milan-raptor-86462.jpeg',
+        'http://4.bp.blogspot.com/-btXyUVef-ps/TsnxFxt-A6I/AAAAAAAABLs/2qFK-7dLjW4/s1600/nick-brandt-wildlife-animal-black-and-white-portraits-buffalo-group-portrait-amboseli-2006.jpg'
 ]
 //'https://i.ytimg.com/vi/3g0reUIKCys/maxresdefault.jpg'
 var index = 0;
