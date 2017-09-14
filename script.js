649edd1252ba555b65f5daaedc8bd4937e3d29e0
Kevin-Velasquez <kevinx23x@yahoo.com>
2017-09-14 02:37:57 -0700

    a few additions

--- script.js
+++ script.js
@@ -10,23 +10,39 @@ var leftButton = document.getElementById("popup");
 var rightButton = document.getElementById("remove");
 var quote = document.getElementById("quote");
 var subtext = document.getElementById("subtext");
+var images = [
+	'http://4.bp.blogspot.com/-btXyUVef-ps/TsnxFxt-A6I/AAAAAAAABLs/2qFK-7dLjW4/s1600/nick-brandt-wildlife-animal-black-and-white-portraits-buffalo-group-portrait-amboseli-2006.jpg',
+	'http://tailandfur.com/wp-content/uploads/2014/09/beautiful-and-cute-animals-wallpaper-2.jpg',
+    'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/caracal-run.ngsversion.1435834128807.adapt.1900.1.jpg',
+    'https://static.pexels.com/photos/86462/red-kite-bird-of-prey-milan-raptor-86462.jpeg',
+    'http://4.bp.blogspot.com/-btXyUVef-ps/TsnxFxt-A6I/AAAAAAAABLs/2qFK-7dLjW4/s1600/nick-brandt-wildlife-animal-black-and-white-portraits-buffalo-group-portrait-amboseli-2006.jpg'
+]
+//'https://i.ytimg.com/vi/3g0reUIKCys/maxresdefault.jpg'
+var index = 0;
 
-leftButton.disabled = false;
+//leftButton.disabled = false;
 
-leftButton.addEventListener("mouseover", function() {
+/*leftButton.addEventListener("mouseover", function() {
     quote.innerHTML = "Click For the current time.";
 });
 
 rightButton.addEventListener("mouseover", function() {
     subtext.innerHTML = "Click to change the background.";
-});
+});*/
+
+rightButton.onmouseover = function() {subtext.innerHTML = "Click to change the background."}
+rightButton.onmouseout = function() {subtext.innerHTML = " "}
+
+leftButton.onmouseover = function() {quote.innerHTML = "Click for the current time."}
+leftButton.onmouseout = function() {quote.innerHTML = " "}
 
 leftButton.addEventListener("click", function() {
     quote.innerHTML = Date();
 });
 
 rightButton.addEventListener("click", function() {
-    document.body.style.backgroundImage = 'url(https://i.ytimg.com/vi/3g0reUIKCys/maxresdefault.jpg)';
-    quote.innerHTML = "Hello";
+    document.body.style.backgroundImage = 'url(' + images[index] + ')';
+    index = (index + 1 < images.length) ? (index + 1) : 0;
+    //quote.innerHTML = "Hello";
     //leftButton.disabled = true;
 });
