ea45b9629ce5d231af59431c2426ee7ee29de125
Anthony Campos <anthonydevelops@gmail.com>
2017-09-13 23:28:11 -0700

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* var = variable 
+document.getElementById = HTML selector targeting the element with an "id" 
+.addEventListener = Method to add an "event" such as "click" or "mouseover"; use a callback function to 
+    quickly provide a response or action for that event that occurs.
+.innerHTML = Changes the HTML text of an element
+.disabled = Method that when true, disables a certain element
+*/
+
 var leftButton = document.getElementById("popup");
 var rightButton = document.getElementById("remove");
 var quote = document.getElementById("quote");
